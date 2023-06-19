7460637ca77e4e26aa8698f1c48b5c54a13b5c60
serNoName <user15b12n5lkuidso2@gmail.com>
2023-06-19 21:39:39 +0300

    modified:   dist/assets/css/style.min.css 	modified:   dist/assets/js/app.min.js 	modified:   dist/index.html 	modified:   gulp/version.json 	modified:   src/index.html 	modified:   src/js/app.js 	modified:   src/js/modules/smoothScroll.js 	modified:   src/scss/media.scss 	modified:   src/scss/style.scss

--- src/js/app.js
+++ src/js/app.js
@@ -4,10 +4,5 @@ import { smoothScroll } from './modules/smoothScroll.js';
 
 webp.testWebP();
 
-const images = $('img.lazyload')
-images.each(function () {
-    $(this).css('height', $(this).attr('height'))
-})
-
 lazyload()
 smoothScroll()
